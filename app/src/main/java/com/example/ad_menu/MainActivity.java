db4b83a7d082f61d797732f387997aac54edf331
sgalvez88 <sergio.galvez.reguera@iestubalcain.net>
2022-11-09 14:25:14 +0100

    cambios en activitis

--- app/src/main/java/com/example/ad_menu/MainActivity.java
+++ app/src/main/java/com/example/ad_menu/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button buttonActividad1;
     Button buttonActividad2;
@@ -22,15 +22,25 @@ public class MainActivity extends AppCompatActivity {
         buttonActividad2 = (Button) findViewById(R.id.buttonActividad2);
         buttonActividad3 = (Button) findViewById(R.id.buttonActividad3);
 
-        buttonActividad1.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Intent intent = new Intent();
-                    }
-                }
-        );
+        buttonActividad1.setOnClickListener(this);
+        buttonActividad2.setOnClickListener(this);
+        buttonActividad3.setOnClickListener(this);
 
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.buttonActividad1:
+                Intent intent = new Intent(this, Activity1.class);
+                startActivity(intent);
+                break;
+            case R.id.buttonActividad2:
+                Intent intent2 = new Intent(this, Activity2.class);
+                break;
+            case R.id.buttonActividad3:
+                Intent intent3 = new Intent(this, Activity3.class);
+                break;
+        }
+    }
 }
